f174e6e97e04be1cf5daab2384b7c8a8cf4165bd
Peter deHaan <peter@deseloper.com>
2021-07-15 15:26:42 -0700

    More better debuggering

--- lib.js
+++ lib.js
@@ -3,7 +3,11 @@ const cp = require("child_process");
 module.exports = npmPack;
 
 function npmPack() {
-  let json = cp.execSync("npm pack --dry-run --json");
+  let json = cp.execSync("npm pack --dry-run --json").toString();
+
+  // Sometimes we get stdout contents before the JSON begins. Weak.
+  json = json.trim().slice(json.indexOf("[") - 1);
+
   json = JSON.parse(json).pop();
   json.files = json.files.sort((a, b) => a.path.localeCompare(b.path));
   return json;
